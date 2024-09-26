c360c0a0446d80e935a340b014db887c8ebf0b57
Criss <151707370+TheGodMrUwU@users.noreply.github.com>
2024-09-26 17:55:08 -0500

    Delete consolgame v.2/consolgame.v2 directory

--- consolgame v.2/consolgame.v2/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Llamar al menú principal al iniciar el juego
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.seleccionarOpcion();
-    }
-}
